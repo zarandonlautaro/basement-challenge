b5a62d1b8749e7ed038255bcd125abd20f001134
zarandonlautaro <zarandonlautaro@mail.com>
2022-02-05 00:57:02 -0300

    fix horizontal bar transform

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,21 +5,25 @@ import header from "../../public/header.svg";
 import asterik from "../../public/asterik.svg";
 import asterikLeft from "../../public/asterik-left.svg";
 
+import styles from "./Header.module.css";
 const Header: React.FC = () => (
   <header className="lg:px-8 lg:py-12">
     <div className="mb-2 sm:mb-16">
-      <Image alt="A man can’t have enough base­ment swag" src={header} />
+      <Image alt="A man can't have enough basement swag" src={header} />
     </div>
     <div className="relative -left-12 lg:-left-12">
-      <hr className="border-1 relative lg:w-screen w-150" />
-      <p className="text-xl lg:text-4xl whitespace-nowrap">
-        A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag
+      <p className={`${styles.marquee}`}>
+        <hr className="border-1  lg:w-screen w-150" />
+        <span>
+          {`A man can't have enough basement swag — A man can't have enough basement swag A man —
+          can't have enough basement swag — A man can't have enough basement swag `}
+        </span>
+        <hr className="border-1  lg:w-screen w-150" />
       </p>
-      <hr className="border-1 relative lg:w-screen w-150" />
       <div className="hidden lg:flex absolute left-32 -bottom-28">
         <Image alt="Asterik" src={asterik} />
       </div>
-      <div className="hidden lg:flex absolute bottom-3 right-5">
+      <div className="hidden lg:flex absolute -bottom-5 right-5">
         <Image alt="Asterik" src={asterikLeft} />
       </div>
     </div>
