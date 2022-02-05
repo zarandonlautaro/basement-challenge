74dd1d68c140b006febf1237126178acf83c3799
zarandonlautaro <zarandonlautaro@mail.com>
2022-02-05 13:36:47 -0300

    react void function component

--- src/components/product/components/Preview.tsx
+++ src/components/product/components/Preview.tsx
@@ -2,29 +2,36 @@ import Image from "next/image";
 import * as React from "react";
 
 import {useAppSelector} from "../../../app/hooks";
+import {Product} from "../types";
 
-const Preview: JSX.Element[] = (): JSX.Element[] => {
-  const products = useAppSelector((state) => state.products);
+const Preview: React.VoidFunctionComponent = (): JSX.Element => {
+  const products: Product[] = useAppSelector((state) => state.products);
 
-  return products.map((product) => (
-    <div key={product.id} className="lg:m-8 lg:p-4 p-4 mx-1 border flex flex-row">
-      <div
-        className="flex w-2/3"
-        style={{
-          background: "linear-gradient(180deg, #1D1D1D 0%, rgba(21, 21, 21, 0) 100%)",
-        }}
-      >
-        <Image alt={product.title} height={231} src={`/${product.image}`} width={234} />
-      </div>
-      <div className="flex flex-col">
-        <p className="text-4xl whitespace-nowrap">{product.title.toUpperCase()}</p>
-        <p className="text-xl whitespace-nowrap" style={{color: "#999999"}}>
-          Unisex Basic Softstyle T-Shirt
-        </p>
-        <p className="text-5xl">{product.price}</p>
-      </div>
-    </div>
-  ));
+  const cardPreview = (
+    <>
+      {products.map((product: Product) => (
+        <div key={product.id} className="lg:m-8 lg:p-4 p-4 mx-1 border flex flex-row">
+          <div
+            className="flex w-2/3"
+            style={{
+              background: "linear-gradient(180deg, #1D1D1D 0%, rgba(21, 21, 21, 0) 100%)",
+            }}
+          >
+            <Image alt={product.title} height={231} src={`/${product.image}`} width={234} />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-4xl whitespace-nowrap">{product.title.toUpperCase()}</p>
+            <p className="text-xl whitespace-nowrap" style={{color: "#999999"}}>
+              Unisex Basic Softstyle T-Shirt
+            </p>
+            <p className="text-5xl">{product.price}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+
+  return cardPreview;
 };
 
 export default Preview;
